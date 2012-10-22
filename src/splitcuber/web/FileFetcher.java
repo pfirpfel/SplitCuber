0ece9e236683063ddb552a0ba3c40d896780b841
pfirpfel <michael@kuenzli.eu>
2012-10-22 13:13:55 +0200

    reformatting added fetchText method

--- src/splitcuber/web/FileFetcher.java
+++ src/splitcuber/web/FileFetcher.java
@@ -1,11 +1,14 @@
 package splitcuber.web;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.DigestInputStream;
@@ -15,9 +18,10 @@ import java.security.NoSuchAlgorithmException;
 import splitcuber.error.WebFetchError;
 
 public class FileFetcher {
-    public static byte[] fetchFile(URL fileURL, ContentType expectedMimeType, FileOutputStream fOut) throws WebFetchError {
+    public static byte[] fetchFile(URL fileURL, ContentType expectedMimeType, FileOutputStream fOut)
+            throws WebFetchError {
         InputStream in = null;
-        
+
         URLConnection conn;
         MessageDigest md;
 
@@ -40,7 +44,7 @@ public class FileFetcher {
             throw new WebFetchError(e.getMessage());
         } finally {
             try {
-                if(in != null)
+                if (in != null)
                     in.close();
                 fOut.flush();
                 fOut.close();
@@ -48,11 +52,11 @@ public class FileFetcher {
                 throw new WebFetchError(e.getMessage());
             }
         }
-        
+
         return md.digest();
     }
-    
-    public static byte[] fetchFile(URL fileURL, ContentType expectedMimeType, File fOut) throws WebFetchError{
+
+    public static byte[] fetchFile(URL fileURL, ContentType expectedMimeType, File fOut) throws WebFetchError {
         FileOutputStream fileStream;
         try {
             fileStream = new FileOutputStream(fOut.getPath());
@@ -60,5 +64,34 @@ public class FileFetcher {
             throw new WebFetchError(e.getMessage());
         }
         return fetchFile(fileURL, expectedMimeType, fileStream);
+    }
+
+    public static String fetchText(String textURL, ContentType expectedMimeType) throws WebFetchError {
+        StringBuilder out = new StringBuilder();
+        String nextLine;
+        URL url = null;
+        URLConnection urlConn = null;
+        InputStreamReader inStream = null;
+        BufferedReader buff = null;
+
+        try {
+            url = new URL(textURL);
+            urlConn = url.openConnection();
+            urlConn.connect();
+            if (!expectedMimeType.doesEqual(urlConn.getContentType())) {
+                throw new WebFetchError("Content-type does not match.");
+            }
+            inStream = new InputStreamReader(urlConn.getInputStream());
+            buff = new BufferedReader(inStream);
+            while ((nextLine = buff.readLine()) != null) {
+                out.append(nextLine);
+            }
+
+        } catch (MalformedURLException e) {
+            throw new WebFetchError("Malformed URL: " + e.getMessage());
+        } catch (IOException e) {
+            throw new WebFetchError("IO problem: " + e.getMessage());
+        }
+        return out.toString();
     }
 }
